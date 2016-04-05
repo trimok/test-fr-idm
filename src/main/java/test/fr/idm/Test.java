90d1a49bbb1e9e978eee25a08824aeed10d7726f
trimok <trimok@mail.fr>
2016-04-05 12:39:19 +0200

    Test java 8

--- src/main/java/test/fr/idm/Test.java
+++ src/main/java/test/fr/idm/Test.java
@@ -1,5 +1,7 @@
 package test.fr.idm;
 
+import java.util.function.Function;
+
 public class Test {
 
 	public static void main(String[] args) {
@@ -7,6 +9,9 @@ public class Test {
 
 		System.out.println("Hello IDM Ready Really");
 
-	}
+		// Test Java 8
+		Function<String, String> personString = p -> "Personne sélectionnée, age =" + p;
+		System.out.println(personString.apply("toto"));
 
+	}
 }
