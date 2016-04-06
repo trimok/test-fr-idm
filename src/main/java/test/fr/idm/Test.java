e95745f3408ad0bb978ad847682b9fe6ab80eb91
trimok <trimok@mail.fr>
2016-04-06 13:46:35 +0200

    RAZ project

--- src/main/java/test/fr/idm/Test.java
+++ src/main/java/test/fr/idm/Test.java
@@ -1,17 +1,11 @@
 package test.fr.idm;
 
-import java.util.function.Function;
 
 public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Hello IDM Ready Really");
-
-		// Test Java 8
-		Function<String, String> personString = p -> "Personne sélectionnée, age =" + p;
-		System.out.println(personString.apply("toto"));
-
+		System.out.println("Hello IDM Test");
 	}
 }
